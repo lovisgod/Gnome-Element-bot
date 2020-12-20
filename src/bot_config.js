4c89ab6263c5a9572897d01dd4d870d1d32f5d2e
lovisgod <olifedayo94@gmail.com>
2020-12-20 21:35:08 +0100

    feature: listen to joinig events and sending messages

--- src/bot_config.js
+++ src/bot_config.js
@@ -4,8 +4,13 @@ import {
     AutojoinRoomsMixin
 } from "matrix-bot-sdk";
 
-const homeserverUrl = "https://gnome.modular.im";
-const accessToken = "MDAxN2xvY2F0aW9uIGdub21lLm9yZwowMDEzaWRlbnRpZmllciBrZXkKMDAxMGNpZCBnZW4gPSAxCjAwMjdjaWQgdXNlcl9pZCA9IEBncmVldC1ib3Q6Z25vbWUub3JnCjAwMTZjaWQgdHlwZSA9IGFjY2VzcwowMDIxY2lkIG5vbmNlID0gR3AzXn5iQSszYi0qdV9nNAowMDJmc2lnbmF0dXJlINibrQKMvj_oOsacRP-rwTAw9qvIflp-rBXtwAHYIPodCg";
+import 'dotenv/config';
+
+
+const homeserverUrl = process.env.HOME_SERVER_URL;
+const accessToken = process.env.ACCESS_TOKEN;
+// const homeserverUrl = "https://gnome.modular.im"
+// const accessToken = "MDAxN2xvY2F0aW9uIGdub21lLm9yZwowMDEzaWRlbnRpZmllciBrZXkKMDAxMGNpZCBnZW4gPSAxCjAwMjdjaWQgdXNlcl9pZCA9IEBncmVldC1ib3Q6Z25vbWUub3JnCjAwMTZjaWQgdHlwZSA9IGFjY2VzcwowMDIxY2lkIG5vbmNlID0gR3AzXn5iQSszYi0qdV9nNAowMDJmc2lnbmF0dXJlINibrQKMvj_oOsacRP-rwTAw9qvIflp-rBXtwAHYIPodCg";
 const storage = new SimpleFsStorageProvider("bot.json");
 
 export {
